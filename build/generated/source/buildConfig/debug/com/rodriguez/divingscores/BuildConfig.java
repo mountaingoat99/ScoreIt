36c49856afd049cf54765358bc57ac048ce53c73
Jeremey Rodriguez <jeremey.rodriguez@outlook.com>
2015-01-23 17:35:49 -0600

    Changed Version Code

--- build/generated/source/buildConfig/debug/com/rodriguez/divingscores/BuildConfig.java
+++ build/generated/source/buildConfig/debug/com/rodriguez/divingscores/BuildConfig.java
@@ -8,11 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.rodriguez.divingscores";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 9;
+  public static final int VERSION_CODE = 10;
   public static final String VERSION_NAME = "";
-  /**
-   * @deprecated Use {@link #APPLICATION_ID}
-   */
-  @Deprecated
-  public static final String PACKAGE_NAME = "com.rodriguez.divingscores";
 }
